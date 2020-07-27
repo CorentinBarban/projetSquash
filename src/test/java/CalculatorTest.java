0648208e5d7d982e420fc7f4ba790f5f743bdeae
cbarban <corentin.barban@sfr.fr>
2020-07-27 09:35:07 +0200

    Gestion du cas de test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,10 +2,12 @@
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.squashtest.ta.galaxia.squash.tf.galaxia.annotations.TFMetadata;
 
 public class CalculatorTest {
 
     @Test
+    @TFMetadata(key = "linked-TC", value = {"ca20783d-8715-4518-8f58-e0a10ea66277"})
     public void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         Assert.assertEquals(2, calculator.add(1, 1));
